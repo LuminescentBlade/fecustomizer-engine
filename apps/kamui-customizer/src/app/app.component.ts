d1e2bee6ca98248427a736fb8b32a1230edad101
Lumi <luminescentblade@gmail.com>
2022-04-17 18:31:11 -0400

    initial commit

---
+++ apps/kamui-customizer/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fecustomizer-engine-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'kamui-customizer';
+}
