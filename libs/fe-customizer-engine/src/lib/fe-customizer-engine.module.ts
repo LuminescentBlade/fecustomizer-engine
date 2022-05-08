4144346806452f77fdce5d3c411764c239286cbc
Lumi <luminescentblade@gmail.com>
2022-05-07 20:07:22 -0400

    update npm package

--- libs/fe-customizer-engine/src/lib/fe-customizer-engine.module.ts
+++ libs/fe-customizer-engine/src/lib/fe-customizer-engine.module.ts
@@ -4,7 +4,6 @@ import { RendererComponent } from './components/renderer/renderer.component';
 import { createCustomElement } from '@angular/elements';
 import { OptionsSelectComponent } from './components/options-select/options-select.component';
 import { LayerRendererComponent } from './components/layer-renderer/layer-renderer.component';
-import { ColorPickerDirective } from './directives/color-picker.directive';
 import { ColorService } from './services/color.service';
 import { ColorSelectComponent } from './components/color-select/color-select.component';
 import { ColorOptionsPaletteComponent } from './components/color-options-palette/color-options-palette.component';
@@ -15,20 +14,21 @@ import { ColorOptionsPaletteComponent } from './components/color-options-palette
     RendererComponent,
     OptionsSelectComponent,
     LayerRendererComponent,
-    ColorPickerDirective,
     ColorSelectComponent,
     ColorOptionsPaletteComponent,
   ],
   exports: [RendererComponent],
+  entryComponents: [RendererComponent],
   providers: [ColorService],
 })
 export class FeCustomizerEngineModule {
-  constructor(private injector: Injector) {}
-
-  ngDoBoostrap() {
+  constructor(private injector: Injector) {
     const customElement = createCustomElement(RendererComponent, {
       injector: this.injector,
     });
-    customElements.define('fec-renderer', customElement);
+    customElements.define('fe-custom-renderer', customElement);
+  }
+
+  ngDoBoostrap() {
   }
 }
